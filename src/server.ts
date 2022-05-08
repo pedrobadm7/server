0cd33fa14cea73ce562f54c14cc05a9b0ea4cc33
Pedro Barros <pedrobars7a@gmail.com>
2022-05-08 17:52:35 -0300

    new port

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(3333, () => {
-  console.log(`Server running on port 3333 🚀`);
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server running on port ${process.env.PORT || 3333} 🚀`);
 })
